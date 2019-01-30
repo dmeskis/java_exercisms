7928548a2e5bac74cfb6eb2eee976aefc1a3550a
Mesky <dmeskis@gmail.com>
2019-01-30 15:05:20 -0700

    Hamming exercism

---
+++ hamming/src/main/java/Hamming.java
@@ -0,0 +1,11 @@
+class Hamming {
+
+    Hamming(String leftStrand, String rightStrand) {
+        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int getHammingDistance() {
+        return 0;
+    }
+
+}
